06664fd96b55498f3a9ba853b69ff497654b5fd8
The-vishnu <143195086+The-vishnu@users.noreply.github.com>
2025-11-13 17:53:41 +0530

    working on backend side

--- app/page.js
+++ app/page.js
@@ -5,16 +5,18 @@ import { Textarea } from "@/components/ui/textarea";
 import { Plus, Loader2, X, Rocket, GrapeIcon, BarChart } from "lucide-react";
 import { useEffect, useState } from "react";
 import toast, { Toaster } from 'react-hot-toast';
+import axios from 'axios';
 
 export default function Home() {
   const AnswerArea = [];
   const [fileName, setFileName] = useState(null);
   const [ansArea, setAnsArea] = useState([]);
+  const [click, setClick] = useState(true);
   const [input, setInput] = useState("");
   const [fileLoading, setFileLoading] = useState(false);
 
   console.log(input);
-  
+
   useEffect(() => {
     if (!fileName) return;
 
@@ -29,8 +31,8 @@ export default function Home() {
     return () => clearTimeout(timer);
   }, [fileName]);
 
-  const handleChooseFile = (e) => {
-    const file = e.target.files[0];
+  const handleChooseFile = async (e) => {
+    const file = e?.target?.files[0];
     if (!file.name.endsWith(".pdf")) {
       toast.error("Only PDF files are allowed!", {
         style: {
@@ -49,8 +51,37 @@ export default function Home() {
 
       return
     }
+
     if (file) {
       setFileName(file);
+      const formData = new FormData();
+      formData.append('pdf', file);
+      setClick(false);
+
+      try {
+        const res = await axios.post("http://localhost:5000/api/upload/pdf",
+          formData,
+          {
+            "Content-Type": "multipart/form-data",
+          }
+        );
+      } catch (error) {
+        console.log("Error in file upload", error);
+        toast.error("File could not be uploaded", {
+          style: {
+            borderRadius: "10px",
+            background: "white", // very light red
+            color: "#b91c1c",       // calm red text
+            border: "1px solid #fecaca", // soft red border
+            fontWeight: 500,
+            boxShadow: "0 2px 6px rgba(0,0,0,0.05)",
+          },
+          iconTheme: {
+            primary: "#ef4444", // red icon
+            secondary: "#fef2f2", // matches background
+          },
+        });
+      }
     }
   };
 
@@ -61,7 +92,7 @@ export default function Home() {
 
   return (
     <>
-      
+
       {input ? (<div className="font-semibold flex flex-col gap-4 p-3.5 h-[80vh] w-[60vw]">
         <div className="text-[1.5rem]">{input}</div>
         <div className="text-[1rem] w-[55vw] h-[50px]">
@@ -75,11 +106,11 @@ export default function Home() {
           </div>
         </div>
         <div className="text-[0.9rem] w-[56vw] v-[50vh] overflow-y-scroll [&::-webkit-scrollbar]:hidden">
-         
+
         </div>
 
       </div>) : (<div className="font-semibold text-2xl">Check your resume score now !</div>)}
-      
+
       <div className="flex items-center gap-3 justify-between font-sans dark:bg-neutral-950 rounded-3xl px-4 py-1 shadow-lg w-[55vw] min-h-[12vh]">
         <Toaster position="top-right" reverseOrder={false} />
         <div className="relative">
@@ -92,13 +123,13 @@ export default function Home() {
                 onClick={handleRemoveFile}
                 className="p-0.5 rounded-full hover:bg-gray-200 dark:hover:bg-neutral-700 transition"
               >
-                <X size={14} className="text-gray-600 dark:text-gray-300" />
+                <X onClick={()=> setClick(true)} size={14} className="text-gray-600 dark:text-gray-300" />
               </button>
             </div>
           )}
           <label htmlFor="fileType"
-            className="flex items-center justify-center w-10 h-10 dark:bg-neutral-800 rounded-full hover:bg-gray-200 dark:hover:bg-neutral-700 cursor-pointer transition-all duration-200 shadow-sm hover:shadow-md">{fileLoading ? <Loader2 className="animate-spin" /> : <Plus />}</label>
-          <input onChange={handleChooseFile} id="fileType" className="hidden" type="file" />
+            className="flex items-center justify-center w-10 h-10 dark:bg-neutral-800 rounded-full hover:bg-gray-200 dark:hover:bg-neutral-700 cursor-pointer transition-all duration-200 shadow-sm hover:shadow-md">{fileLoading ? <Loader2 className="animate-spin" /> : <Plus className={`${click ? '' : 'text-gray-300'}`}/>}</label>
+          <input onChange={handleChooseFile} disabled={!!fileName} id="fileType" className="hidden" type="file" />
         </div>
 
         <Textarea
